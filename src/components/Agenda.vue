6bd35abbd80e50f408d0b733f852fed6d6db1554
Daniel Lima <daniel.silvalima89@gmail.com>
2025-01-25 19:34:18 -0300

    2025-01-25

--- src/components/Agenda.vue
+++ src/components/Agenda.vue
@@ -110,12 +110,12 @@
 
 <script >
 import { mapGetters } from "vuex";
-//import SideMenu from "@/components/SideMenu.vue";
+import SideMenu from "@/components/SideMenu.vue";
 import { VDateInput } from 'vuetify/labs/VDateInput';
 
 export default {
   components: {
-    SideMenu: () => import('@/components/SideMenu.vue'),
+    SideMenu,
     VDateInput
   },
   data() {
